97bf4697f3c5b337a7e98fa83b0c425585724960
GuzmanVera <guzvera.liceo@gmail.com>
2023-08-15 15:39:14 -0300

    primer entrega .NET

---
+++ Practico1/Practico1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Shared;
+using DataAccessLayer;
+using DataAccessLayer.DAL;
+using DataAccessLayer.IDALs;
+using System.Globalization;
+
+IDAL_Persona idal = new DAL_Persona();
+do
+{
+    try
+    { 
+        Persona per = new Persona();
+
+        Console.WriteLine("Nombre:");
+        per.nombre = Console.ReadLine();
+
+        Console.WriteLine("Apellido:");
+        per.apellido = Console.ReadLine();
+
+        Console.WriteLine("Documento:");
+        per.Documento = Console.ReadLine();
+
+        Console.Write("Fecha de nacimiento (dd/mm/yyyy): ");
+        string fechaNacString = Console.ReadLine();
+        per.fechaNac = DateTime.ParseExact(fechaNacString, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+        idal.Insert(per);
+
+    }catch(Exception ex)
+    {
+        Console.WriteLine("Error:" + ex.Message);
+    }
+
+    Console.WriteLine("Desea seguir? S|N");
+
+} while(Console.ReadLine()!= "N");
+
+var personasOrdenadas = idal.GetPersonasOrdenadasPorFechaNacimiento();
+
+foreach (var persona in personasOrdenadas)
+{
+    Console.WriteLine($"Nombre: {persona.nombre}, Fecha de Nacimiento: {persona.fechaNac}");
+}
+
+
---
+++ Practico1/Shared/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Shared
+{
+    public class Persona
+    {
+        public String nombre { get; set; } = "--Sin Nombre--";
+        public String apellido { get; set; } = "--Sin Apellido--";
+
+        private String documento = "";
+        public String Documento
+        {
+            get { return documento; }
+            set
+            {
+                if (value.Length < 7)
+                {
+                    throw new Exception("Documento incorrecto");
+                }
+                else
+                {
+                    documento = value.ToUpper();
+                }
+            }
+        }
+
+        public DateTime fechaNac { get; set; }
+    }
+}
